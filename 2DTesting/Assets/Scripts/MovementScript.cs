3307786494c14c07298a9a06be21c0f88bb4291b
Harry Liuson <hliuson@hotmail.com>
2021-02-23 10:37:41 -0800

    cleaned up the repo

--- 2DTesting/Assets/Scripts/MovementScript.cs
+++ 2DTesting/Assets/Scripts/MovementScript.cs
@@ -11,9 +11,12 @@ public class MovementScript : MonoBehaviour {
 
     float horizontalSpeed = 0, verticalSpeed = 0;
 
-    float diagonalLimit = 0.707f;
-    public float speed = 20.0f;
-    public float acceleration = 5.0f;
+    const float diagonalLimit = 0.707f;
+    public float speed = 10.0f;
+    public float acceleration = 2.0f;
+    const float regSpeed = 10.0f;
+    const float diagSpeed = diagonalLimit * regSpeed;
+
     // Start is called before the first frame update
     void Start() {
         body = GetComponent<Rigidbody2D>();
@@ -24,12 +27,29 @@ public class MovementScript : MonoBehaviour {
         int verticalDirection = (int)((verticalSpeed / Math.Abs(verticalSpeed)));
 
         if (horizontalPress != 0 && verticalPress != 0) {
-            horizontalPress *= diagonalLimit;
-            verticalPress *= diagonalLimit;
-        } else if (horizontalPress == 0 & verticalPress == 0)
+            //horizontalSpeed *= diagonalLimit;
+            //verticalSpeed *= diagonalLimit;
+            speed = diagSpeed;
+        } else if (horizontalPress == 0 && verticalPress == 0)
         {
-            horizontalSpeed = roundUp(horizontalSpeed, 1);
-            verticalSpeed = roundUp(verticalSpeed, 1);
+            horizontalSpeed = (float)Math.Floor(horizontalSpeed);
+            verticalSpeed = (float)Math.Floor(verticalSpeed);
+            //Because we can't have nice things, and also I can't do math right now
+            if (Math.Abs(horizontalSpeed) == 1) { horizontalSpeed = 0; }
+            if (Math.Abs(verticalSpeed) == 1) { verticalSpeed = 0; }
+        } else
+        {
+            speed = regSpeed;
+        }
+        
+        if (horizontalSpeed > speed)
+        {
+            horizontalSpeed = speed;
+        }
+
+        if (verticalSpeed > speed)
+        {
+            verticalSpeed = speed;
         }
 
         if ((horizontalPress == 0) && Math.Abs(horizontalSpeed) > 0)
@@ -64,15 +84,6 @@ public class MovementScript : MonoBehaviour {
     void Update() {
         horizontalPress = Input.GetAxisRaw("Horizontal");
         verticalPress = Input.GetAxisRaw("Vertical");
-    }
-
-    float roundUp(float num, int factor)
-    {
-        if(num < 0)
-        {
-            num = -num;
-            return -(num + factor - 1 - (num + factor - 1) % factor);
-        }
-        return num + factor - 1 - (num + factor - 1) % factor;
+        body.constraints = RigidbodyConstraints2D.FreezeRotation;
     }
 }
