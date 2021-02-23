8e00df9c7d84637cea166877aa7dc787d7cb0483
Danny Thornewell <DannyT23@lakesideschool.org>
2021-02-23 09:55:16 -0800

    New movement script

---
+++ 2DTesting/Assets/WallCollision.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Just hit another collider 2D
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Hit");
+    }
+
+    //Hitting a collider 2D
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        //Do something
+    }
+
+    //Just stop hitting a collider 2D
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        //Do something
+    }
+}
