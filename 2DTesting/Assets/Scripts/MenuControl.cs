ab6e7048ee60f3439c9953c91d521b2553badcc9
Danny Thornewell <DannyT23@lakesideschool.org>
2021-02-25 10:23:35 -0800

    Added quit button to first level

---
+++ 2DTesting/Assets/Scripts/MenuControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+    // Update is called once per frame
+    public void test()
+    {
+        Debug.Log("Pressed");
+    }
+
+    public void quitToMenu()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    void Update()
+    {
+        
+    }
+}
