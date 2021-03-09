377729fbde6bf7fcf60222ad52d402bf463893a4
Harry Liuson <hliuson@hotmail.com>
2021-03-09 10:24:32 -0800

    Enemy class

---
+++ 2DTesting/Assets/Scripts/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : Combatable
+{
+    public float aggressionRadius;
+    public Combatable currentTarget;
+    public float speed;
+    public void checkAggression()
+    {
+        //check for player withing aggression radius
+        //assign player as currentTarget
+    }
+}
