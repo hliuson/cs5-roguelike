7d419ccdc6575d1fc474f7464ba2ba687afc4c5d
Danny Thornewell <DannyT23@lakesideschool.org>
2021-03-16 10:24:05 -0700

    Added get target function based on closest player

--- 2DTesting/Assets/Scripts/Enemy.cs
+++ 2DTesting/Assets/Scripts/Enemy.cs
@@ -4,12 +4,49 @@ using UnityEngine;
 
 public abstract class Enemy : Combatable
 {
+    //For two players or anything helping player
+    public Combatable[] targets;
     public float aggressionRadius;
     public Combatable currentTarget;
     public float speed;
-    public void checkAggression()
+    private void checkAggression()
     {
-        //check for player withing aggression radius
-        //assign player as currentTarget
+        Combatable closestEnemy = GetClosestEnemy(targets);
+        if (Vector2.Distance(closestEnemy.transform.position, transform.position)  < aggressionRadius)
+        {
+            currentTarget = closestEnemy;
+        } else
+        {
+            currentTarget = null;
+        }
+    }
+
+    private Combatable GetClosestEnemy(Combatable[] enemies)
+    {
+        Combatable closest = null;
+        float minDist = Mathf.Infinity;
+        Vector2 currentPos = transform.position;
+        foreach (Combatable c in enemies)
+        {
+            float dist = Vector2.Distance(c.transform.position, currentPos);
+            if (dist < minDist)
+            {
+                closest = c;
+                minDist = dist;
+            }
+        }
+        return closest;
+    }
+
+    void Update()
+    {
+        //Get all objects in the scene
+        Object[] allObjects = (UnityEngine.Object.FindObjectsOfType(typeof(Combatable)));
+        Combatable[] temp = new Combatable[allObjects.Length];
+        for(int g = 0; g < allObjects.Length; g++)
+        {
+            temp[g] = ((GameObject)allObjects[g]).GetComponent<Combatable>();
+        }
+        targets = temp;
     }
 }
