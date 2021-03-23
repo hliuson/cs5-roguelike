b2c592392cd6649951fff1ec78f01448328c3392
Harry Liuson <hliuson@hotmail.com>
2021-03-22 20:49:12 -0700

    this works.

---
+++ 2DTesting/Assets/Scripts/Heap.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+//From https://www.youtube.com/playlist?list=PLFt_AvWsXl0cq5Umv3pMC9SPnKjfp9eGW
+//No idea what is actually happening here, it just seemed to work and was needed to get to the next part of the tutorial
+public class Heap<type> where type : IHeapItem<type>
+{
+
+	type[] items;
+	int currentItemCount;
+
+	public Heap(int maxHeapSize)
+	{
+		items = new type[maxHeapSize];
+	}
+
+	public void Add(type item)
+	{
+		item.HeapIndex = currentItemCount;
+		items[currentItemCount] = item;
+		SortUp(item);
+		currentItemCount++;
+	}
+
+	public type RemoveFirst()
+	{
+		type firstItem = items[0];
+		currentItemCount--;
+		items[0] = items[currentItemCount];
+		items[0].HeapIndex = 0;
+		SortDown(items[0]);
+		return firstItem;
+	}
+
+	public void UpdateItem(type item)
+	{
+		SortUp(item);
+	}
+
+	public int Count
+	{
+		get
+		{
+			return currentItemCount;
+		}
+	}
+
+	public bool Contains(type item)
+	{
+		return Equals(items[item.HeapIndex], item);
+	}
+
+	void SortDown(type item)
+	{
+		while (true)
+		{
+			int childIndexLeft = item.HeapIndex * 2 + 1;
+			int childIndexRight = item.HeapIndex * 2 + 2;
+			int swapIndex = 0;
+
+			if (childIndexLeft < currentItemCount)
+			{
+				swapIndex = childIndexLeft;
+
+				if (childIndexRight < currentItemCount)
+				{
+					if (items[childIndexLeft].CompareTo(items[childIndexRight]) < 0)
+					{
+						swapIndex = childIndexRight;
+					}
+				}
+
+				if (item.CompareTo(items[swapIndex]) < 0)
+				{
+					Swap(item, items[swapIndex]);
+				}
+				else
+				{
+					return;
+				}
+
+			}
+			else
+			{
+				return;
+			}
+
+		}
+	}
+
+	void SortUp(type item)
+	{
+		int parentIndex = (item.HeapIndex - 1) / 2;
+
+		while (true)
+		{
+			type parentItem = items[parentIndex];
+			if (item.CompareTo(parentItem) > 0)
+			{
+				Swap(item, parentItem);
+			}
+			else
+			{
+				break;
+			}
+
+			parentIndex = (item.HeapIndex - 1) / 2;
+		}
+	}
+
+	void Swap(type itemA, type itemB)
+	{
+		items[itemA.HeapIndex] = itemB;
+		items[itemB.HeapIndex] = itemA;
+		int itemAIndex = itemA.HeapIndex;
+		itemA.HeapIndex = itemB.HeapIndex;
+		itemB.HeapIndex = itemAIndex;
+	}
+
+
+
+}
+
+public interface IHeapItem<type> : IComparable<type>
+{
+	int HeapIndex
+	{
+		get;
+		set;
+	}
+}
---
+++ 2DTesting/Assets/Scripts/Tracker.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//From https://www.youtube.com/playlist?list=PLFt_AvWsXl0cq5Umv3pMC9SPnKjfp9eGW
+public class Tracker : MonoBehaviour {
+	
+	public Transform target;
+	public float speed = 20;
+
+	Vector2[] path;
+	int targetIndex;
+
+	void Start() {
+		StartCoroutine (refreshPath ());
+	}
+
+	IEnumerator refreshPath() {
+		Vector2 targetPositionOld = (Vector2)target.position + Vector2.up; // ensure not target.position initially
+			
+		while (true) {
+			if (targetPositionOld != (Vector2)target.position) {
+				targetPositionOld = (Vector2)target.position;
+
+				path = Pathfinding.requestPath(transform.position, target.position);
+				StopCoroutine ("FollowPath");
+				StartCoroutine ("FollowPath");
+			}
+			//Yeild is used for iterators 
+			yield return new WaitForSeconds (.25f);
+		}
+	}
+		
+	IEnumerator followPath() {
+		if (path.Length > 0) {
+			targetIndex = 0;
+			Vector2 currentWaypoint = path [0];
+
+			while (true) {
+				if ((Vector2)transform.position == currentWaypoint) {
+					targetIndex++;
+					if (targetIndex >= path.Length) {
+						yield break;
+					}
+					currentWaypoint = path [targetIndex];
+				}
+
+				transform.position = Vector2.MoveTowards (transform.position, currentWaypoint, speed * Time.deltaTime);
+				yield return null;
+
+			}
+		}
+	}
+
+	public void OnDrawGizmos() {
+		if (path != null) {
+			for (int i = targetIndex; i < path.Length; i ++) {
+				Gizmos.color = Color.black;
+				Gizmos.DrawCube((Vector3)path[i], Vector3.one *.5f);
+
+				if (i == targetIndex) {
+					Gizmos.DrawLine(transform.position, path[i]);
+				}
+				else {
+					Gizmos.DrawLine(path[i-1],path[i]);
+				}
+			}
+		}
+	}
+}
